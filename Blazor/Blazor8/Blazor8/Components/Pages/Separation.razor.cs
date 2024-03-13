1da4035587f22c240df453d82433cc12bfb7d57e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-03-13 12:50:22 -0300

    S07E02 - Separar CSharp

---
+++ Blazor/Blazor8/Blazor8/Components/Pages/Separation.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor8.Components.Pages
+{
+    public partial class Separation
+    {
+        /*
+        * Razor (Engine Template - ASP.NET): HTML/CSS/JS + C#
+        * 
+        * Blazor: .razor (componentes blazor)
+        * ASP.NET .cshtml (MVC & Razor Pages)
+        */
+        
+        public string Texto = "Oi eu sou código CSharp!";
+
+    }
+}
