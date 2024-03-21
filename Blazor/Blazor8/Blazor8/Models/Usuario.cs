86658dcce3908b0d2d8cd1b730ffd47b72679fba
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-03-21 07:53:49 -0300

    S09E02 - Formulário - Parte 2 - EditForm

---
+++ Blazor/Blazor8/Blazor8/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor8.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "O campo 'E-mail' é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo 'E-mail' é inválido!")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "O campo 'Senha' é obrigatório!")]
+        [MinLength(8, ErrorMessage = "O campo 'Senha' deve ter pelo menos 8 caracteres!")]
+        public string? Senha { get; set; }
+    }
+}
