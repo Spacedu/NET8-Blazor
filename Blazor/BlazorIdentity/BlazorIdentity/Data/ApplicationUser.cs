9e640458252097b5ae2baf38d9dabf3baafe581a
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-03-22 10:57:42 -0300

    S10E03 - Configurar e Executar Projeto com Identity

--- Blazor/BlazorIdentity/BlazorIdentity/Data/ApplicationUser.cs
+++ Blazor/BlazorIdentity/BlazorIdentity/Data/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace BlazorIdentity.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? Name { get; set; }
+        public DateTime Birth { get; set; }
     }
 
 }
---
+++ Blazor/BlazorIdentity/BlazorIdentity/Data/Migrations/20240322134809_AddColumnsNameBirthToApplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorIdentity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnsNameBirthToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
