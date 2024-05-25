cffdfde48583a8de5b725764b2889d8a45fb0cbf
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-05-25 11:52:07 -0300

    S14E08 - Domain - Company

---
+++ Blazor/Gestao.Domain/Company.cs
@@ -0,0 +1,26 @@
+﻿using Gestao.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao.Domain
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string LegalName { get; set; } = string.Empty;
+        public string TradeName { get; set; } = string.Empty;
+        public string TaxId { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Neighborhood { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Complement { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
