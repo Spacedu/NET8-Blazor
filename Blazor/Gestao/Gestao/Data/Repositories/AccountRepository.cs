0a9c550c529b6223a68826ed7ea9eac0ece84230
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-05-30 10:08:12 -0300

    S14E17 - AccountRepository

--- Blazor/Gestao/Gestao/Data/Repositories/AccountRepository.cs
+++ Blazor/Gestao/Gestao/Data/Repositories/AccountRepository.cs
@@ -1,6 +1,56 @@
-﻿namespace Gestao.Data.Repositories
+﻿using Gestao.Client.Libraries.Utilities;
+using Gestao.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Data.Repositories
 {
-    public class AccountRepository
+    public class AccountRepository : IAccountRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public AccountRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<PaginatedList<Account>> GetAll(int companyId, int pageIndex, int pageSize)
+        {
+            var items = await _db.Accounts.Where(a => a.CompanyId == companyId).
+                Skip((pageIndex - 1) * pageSize).
+                Take(pageSize)
+                .ToListAsync();
+
+            var count = await _db.Accounts.Where(a => a.CompanyId == companyId).CountAsync();
+            int totalPages = (int)Math.Ceiling((decimal)count / pageSize);
+
+            return new PaginatedList<Account>(items, pageIndex, totalPages);
+        }
+        public async Task<List<Account>> GetAll(int companyId)
+        {
+            return await _db.Accounts.Where(a => a.CompanyId == companyId).ToListAsync();
+        }
+        public async Task<Account?> Get(int id)
+        {
+            return await _db.Accounts.SingleOrDefaultAsync(a => a.Id == id);
+        }
+        public async Task Add(Account entity)
+        {
+            _db.Accounts.Add(entity);
+            await _db.SaveChangesAsync();
+        }
+        public async Task Update(Account entity)
+        {
+            _db.Accounts.Update(entity);
+            await _db.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await Get(id);
+
+            if (entity is not null)
+            {
+                _db.Accounts.Remove(entity);
+                await _db.SaveChangesAsync();
+            }
+        }
     }
 }
---
+++ Blazor/Gestao/Gestao/Data/Repositories/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gestao.Client.Libraries.Utilities;
+using Gestao.Domain;
+
+namespace Gestao.Data.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task Add(Account entity);
+        Task Delete(int id);
+        Task<Account?> Get(int id);
+        Task<List<Account>> GetAll(int companyId);
+        Task<PaginatedList<Account>> GetAll(int companyId, int pageIndex, int pageSize);
+        Task Update(Account entity);
+    }
+}
