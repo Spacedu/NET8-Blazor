62ef57d4ea437a4a745587409b02d1bbfe80f368
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-06-06 13:08:16 -0300

    S14E48 - Company - Add - Parte 8 - CNPJ Validação e Tradução de Mensagens

--- Blazor/Gestao.Domain/Company.cs
+++ Blazor/Gestao.Domain/Company.cs
@@ -5,29 +5,31 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gestao.Domain.Libraries.Validations;
 
 namespace Gestao.Domain
 {
     public class Company
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo 'Razão Social' é obrigatório!")]
         public string LegalName { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Nome fantasia' é obrigatório!")]
         public string TradeName { get; set; } = string.Empty;
-        [Required]
+        [CNPJ(ErrorMessage = "O campo 'CNPJ' é inválido!")]
+        [Required(ErrorMessage = "O campo 'CNPJ' é obrigatório!")]
         public string TaxId { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'CEP' é obrigatório!")]
         public string PostalCode { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Estado' é obrigatório!")]
         public string State { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Cidade' é obrigatório!")]
         public string City { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Bairro' é obrigatório!")]
         public string Neighborhood { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Endereço' é obrigatório!")]
         public string Address { get; set; } = string.Empty;
-        [Required]
+        [Required(ErrorMessage = "O campo 'Complemento' é obrigatório!")]
         public string Complement { get; set; } = string.Empty;
         public DateTimeOffset CreatedAt { get; set; }
         public Guid UserId { get; set; }
