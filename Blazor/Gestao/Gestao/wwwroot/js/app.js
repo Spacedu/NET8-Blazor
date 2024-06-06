25986629ccafd572a6931a834d697441b3c735dc
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-06-06 10:51:34 -0300

    S14E43 - Company - Add - Parte 3 - Mascara para CNPJ e CEP

---
+++ Blazor/Gestao/Gestao/wwwroot/js/app.js
@@ -0,0 +1,8 @@
+﻿function CompanyMaskInit() {
+    var cleaveCNPJ, cleaveCEP;
+    //CEP: 00.000-000
+    //CNPJ: 00.000.000/0001-00
+
+    document.querySelector("#cnpj") && (cleaveDelimiters = new Cleave("#cnpj", { delimiters: [".", ".", "/", "-"], blocks: [2, 3, 3, 4, 2], uppercase: !0 }))
+    document.querySelector("#cep") && (cleaveDelimiters = new Cleave("#cep", { delimiters: [".", "-"], blocks: [2, 3, 3], uppercase: !0 }))
+}
