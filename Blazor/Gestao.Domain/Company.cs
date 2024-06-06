6dfbf592cff80a37d5cb105a31d096414e306050
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-06-06 13:23:23 -0300

    S14E48 - Company - Add - Parte 9 - Validação MinimumLength

--- Blazor/Gestao.Domain/Company.cs
+++ Blazor/Gestao.Domain/Company.cs
@@ -13,24 +13,47 @@ namespace Gestao.Domain
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "O campo 'Razão Social' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O campo 'Razão Social' deve ter pelo menos {1} caracteres!")]
         public string LegalName { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Nome fantasia' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O campo 'Nome fantasia' deve ter pelo menos {1} caracteres!")]
         public string TradeName { get; set; } = string.Empty;
+
+
         [CNPJ(ErrorMessage = "O campo 'CNPJ' é inválido!")]
         [Required(ErrorMessage = "O campo 'CNPJ' é obrigatório!")]
         public string TaxId { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'CEP' é obrigatório!")]
+        [MinLength(10, ErrorMessage = "O campo 'CEP' deve ter {1} caracteres!")]
         public string PostalCode { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Estado' é obrigatório!")]
         public string State { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Cidade' é obrigatório!")]
         public string City { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Bairro' é obrigatório!")]
         public string Neighborhood { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Endereço' é obrigatório!")]
+        [MinLength(10, ErrorMessage = "O campo 'Endereço' deve ter pelo menos {1} caracteres!")]
         public string Address { get; set; } = string.Empty;
+
+
         [Required(ErrorMessage = "O campo 'Complemento' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O campo 'Complemento' deve ter pelo menos {1} caracteres!")]
         public string Complement { get; set; } = string.Empty;
+
+
         public DateTimeOffset CreatedAt { get; set; }
         public Guid UserId { get; set; }
         public ApplicationUser User { get; set; } = null!;
