5ecfac664ec69b121e9455bdcfbe4dd3674c31e6
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-06-06 14:10:19 -0300

    S14E50 - Company - Add - Refatorar Obter UserId

---
+++ Blazor/Gestao/Gestao.Client/Libraries/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Gestao.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Gestao.Client.Libraries.Helpers
+{
+    public class AuthenticationHelper
+    {
+        public static async Task<Guid?> GetAuthenticationUserIdAsync(AuthenticationStateProvider authenticationStateProvider)
+        {
+            var authenticationState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var userId = authenticationState.User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
+
+            if (userId != null && Guid.TryParse(userId!.Value, out var authenticationUserId))
+            {
+                return authenticationUserId;
+            }
+            return null;
+        }
+    }
+}
