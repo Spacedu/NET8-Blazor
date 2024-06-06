de9f6e55d73781e84f44e7739d8125cfe6f96619
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-06-06 11:19:36 -0300

    S14E44 - Company - Add - Parte 4 - Adaptação ao Blazor

--- Blazor/Gestao.Domain/Company.cs
+++ Blazor/Gestao.Domain/Company.cs
@@ -1,6 +1,7 @@
 ﻿using Gestao.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,23 @@ namespace Gestao.Domain
     public class Company
     {
         public int Id { get; set; }
+        [Required]
         public string LegalName { get; set; } = string.Empty;
+        [Required]
         public string TradeName { get; set; } = string.Empty;
+        [Required]
         public string TaxId { get; set; } = string.Empty;
+        [Required]
         public string PostalCode { get; set; } = string.Empty;
+        [Required]
         public string State { get; set; } = string.Empty;
+        [Required]
         public string City { get; set; } = string.Empty;
+        [Required]
         public string Neighborhood { get; set; } = string.Empty;
+        [Required]
         public string Address { get; set; } = string.Empty;
+        [Required]
         public string Complement { get; set; } = string.Empty;
         public DateTimeOffset CreatedAt { get; set; }
         public Guid UserId { get; set; }
