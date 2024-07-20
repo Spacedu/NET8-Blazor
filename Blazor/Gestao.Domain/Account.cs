5d058d52ed4c458319bd9247f35cd2639c79fe69
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-07-20 17:39:10 -0300

    S17E04 - Contas - Cadastro - Validação

--- Blazor/Gestao.Domain/Account.cs
+++ Blazor/Gestao.Domain/Account.cs
@@ -1,6 +1,7 @@
 ﻿using Gestao.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,12 @@ namespace Gestao.Domain
     public class Account : ISoftDelete
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo 'Descrição/Nome' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O campo 'Descrição/Nome' deve ter pelo menos {1} caracteres!")]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O campo 'Saldo inicial' é obrigatório!")]
         public decimal Balance { get; set; }
+        [Required(ErrorMessage = "O campo 'Data de abertura da conta' é obrigatório!")]
         public DateTimeOffset BalanceDate { get; set; }
         public int? CompanyId { get; set; }
         public Company? Company { get; set; }
