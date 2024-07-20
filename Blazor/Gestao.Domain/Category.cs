211c0f9efffb3a79d9591ba43e214b2dccaa04f4
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-07-20 19:21:28 -0300

    S18E03 - Categoria - Cadastro

--- Blazor/Gestao.Domain/Category.cs
+++ Blazor/Gestao.Domain/Category.cs
@@ -1,10 +1,13 @@
 ﻿using Gestao.Domain.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gestao.Domain
 {
     public class Category : ISoftDelete
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo 'Nome' é obrigatório!")]
+        [MinLength(3, ErrorMessage = "O campo 'Nome' deve ter pelo menos {1} caracteres!")]
         public string Name { get; set; } = null!;
         public int? CompanyId { get; set; }
         public Company? Company { get; set; }
