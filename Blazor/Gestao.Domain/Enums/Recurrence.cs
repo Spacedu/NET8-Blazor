3ba8e770c443947d0c26d44cc685c4144ca84dab
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-08-06 13:38:43 -0300

    S19E09 - Financial - Cadastro - Lista Repetição Traduzir

--- Blazor/Gestao.Domain/Enums/Recurrence.cs
+++ Blazor/Gestao.Domain/Enums/Recurrence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,13 @@ namespace Gestao.Domain.Enums
 {
     public enum Recurrence
     {
+        [Display(Name = "Não")]
         None,
+        [Display(Name = "Semanalmente")]
         Weekly,
+        [Display(Name = "Mensalmente")]
         Monthly,
+        [Display(Name = "Anualmente")]
         Yearly
     }
 }
---
+++ Blazor/Gestao/Gestao.Client/Libraries/Extensions/EnumExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Gestao.Client.Libraries.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            //Atributo
+            var enumType = enumValue.GetType();
+            var memberInfo = enumType.GetMember(enumValue.ToString());
+
+            if(memberInfo.Length > 0)
+            {
+                var displayAttribute = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
+                if (displayAttribute != null)
+                {
+                    if(displayAttribute.Name != null)
+                        return displayAttribute.Name;
+                }
+            }
+
+            //None, Weekly....
+            return enumValue.ToString();
+        }
+    }
+}
