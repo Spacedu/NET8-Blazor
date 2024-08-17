b3adeb5209205fd6c948fa5ae3e30dd9e17ea0ce
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-08-17 12:56:59 -0300

    S19E23 - Financial - Cadastro - Teste final

--- Blazor/Gestao/Gestao/Data/Migrations/20240817155203_DatabaseV7.0.Designer.cs
+++ Blazor/Gestao/Gestao/Data/Migrations/20240817155203_DatabaseV7.0.Designer.cs
@@ -4,6 +4,7 @@ using Gestao.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gestao.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240817155203_DatabaseV7.0")]
+    partial class DatabaseV70
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
