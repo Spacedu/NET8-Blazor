c2476101a66b04100d3850dc911bc4fb1248989f
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-08-18 13:16:52 -0300

    S19E29 - Financial - Converter Texto Conta a Pagar e receber

---
+++ Blazor/Gestao/Gestao.Client/Libraries/Converters/TypeFinancialTransactionToString.cs
@@ -0,0 +1,16 @@
+﻿using Gestao.Domain.Enums;
+
+namespace Gestao.Client.Libraries.Converters
+{
+    public class TypeFinancialTransactionToString
+    {
+        public static string Converter(string type)
+        {
+            return type == TypeFinancialTransaction.Pay.ToString() ? "pagamento" : "recebimento";
+        }
+        public static string ConverterInfinitive(string type)
+        {
+            return type == TypeFinancialTransaction.Pay.ToString() ? "pagar" : "receber";
+        }
+    }
+}
