ad7dfddecd7b3d8effbc6fd28e68c22adbe444ae
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2024-09-03 17:55:46 -0300

    S21E03 - Painel - Parte 2 - Ajustes de Layout

---
+++ Blazor/Gestao/Gestao/Components/Pages/PageBase.cs
@@ -0,0 +1,12 @@
+﻿using Blazored.LocalStorage;
+using Gestao.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Gestao.Components.Pages
+{
+    public class PageBase : ComponentBase
+    {
+        [Inject] public ILocalStorageService LocalStorage { get; set; } = null!;
+        [Inject] public ApplicationDbContext DB { get; set; } = null!;
+    }
+}
